b3df287dd4077c8415aaca383bdf516898de97d2
Chinmay Tare <chinmay.tare1232@gmail.com>
2018-08-17 16:05:27 +0530

    Object methods

---
+++ objects/objects-methods.js
@@ -0,0 +1,24 @@
+let restaurant = {
+    name: 'Chinmay coffee shop',
+    guestCapacity: 75,
+    guestCount: 0,
+    checkAvailability: function (partySize) {
+        let seatsLeft = this.guestCapacity - this.guestCount
+        return partySize <= seatsLeft
+    },
+    seatParty: function (partySize) {
+        this.guestCount = this.guestCount + partySize
+    },
+    removeParty: function (partySize) {
+        this.guestCount = this.guestCount - partySize
+    }
+        
+}
+
+restaurant.seatParty(72)
+//console.log(restaurant)
+console.log(restaurant.checkAvailability(4))
+restaurant.removeParty(3)
+//console.log(restaurant)
+console.log(restaurant.checkAvailability(7))
+

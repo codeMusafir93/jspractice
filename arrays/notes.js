f37afaef89d906524b2ab9e58574c51f89052197
Chinmay Tare <savvychinmay@gmail.com>
2018-08-30 17:16:12 +0530

    Arrays methods

---
+++ arrays/notes.js
@@ -0,0 +1,13 @@
+const notes = ['Note 1', 'Note 2', 'Note 3']
+
+// notes.pop() // Push method
+// notes.push('My new note') // Pop method
+// console.log(notes.shift())
+// notes.unshift('My first note')
+// // console.log(notes.length)
+
+// notes.splice(1, 1, 'This is the new second item')
+
+notes[2] = "This is the new note 3"
+console.log(notes)
+
